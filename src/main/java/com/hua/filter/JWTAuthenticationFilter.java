c6ef5590c66174582c9d4a3ab066d0fb6be83327
dream <dream@dreamdeMacBook-Pro.local>
2019-12-10 22:18:16 +0800

    0.0.2 change taken to body

--- src/main/java/com/hua/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/hua/filter/JWTAuthenticationFilter.java
@@ -26,58 +26,58 @@ import java.util.Collection;
  */
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    private ThreadLocal<Integer> rememberMe = new ThreadLocal<>();
-    private AuthenticationManager authenticationManager;
+    private final ThreadLocal<Integer> rememberMe = new ThreadLocal<>();
+    private final AuthenticationManager authenticationManager;
 
-    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+    public JWTAuthenticationFilter(final AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
         super.setFilterProcessesUrl("/auth/login");
     }
 
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request,
-                HttpServletResponse response) throws AuthenticationException {
+    public Authentication attemptAuthentication(final HttpServletRequest request, final HttpServletResponse response)
+            throws AuthenticationException {
 
-            LoginUser loginUser = new LoginUser();
-            loginUser.setPassword(request.getParameter("password"));
-            loginUser.setUsername(request.getParameter("username"));
-            loginUser.setRememberMe(Integer.parseInt(request.getParameter("rememberMe")));
-            System.out.println(loginUser.toString());
-            rememberMe.set(loginUser.getRememberMe());
-            return authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(loginUser.getUsername(), loginUser.getPassword(), new ArrayList<>())
-            );
-        
+        final LoginUser loginUser = new LoginUser();
+        loginUser.setPassword(request.getParameter("password"));
+        loginUser.setUsername(request.getParameter("username"));
+        loginUser.setRememberMe(Integer.parseInt(request.getParameter("rememberMe")));
+        System.out.println(loginUser.toString());
+        rememberMe.set(loginUser.getRememberMe());
+        return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUser.getUsername(),
+                loginUser.getPassword(), new ArrayList<>()));
+
     }
 
     // 成功验证后调用的方法
     // 如果验证成功，就生成token并返回
     @Override
-    protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain chain,
-                                            Authentication authResult) throws IOException, ServletException {
+    protected void successfulAuthentication(final HttpServletRequest request, final HttpServletResponse response,
+            final FilterChain chain, final Authentication authResult) throws IOException, ServletException {
 
-        JwtUser jwtUser = (JwtUser) authResult.getPrincipal();
+        final JwtUser jwtUser = (JwtUser) authResult.getPrincipal();
         System.out.println("jwtUser:" + jwtUser.toString());
-        boolean isRemember = rememberMe.get() == 1;
+        final boolean isRemember = rememberMe.get() == 1;
 
         String role = "";
-        Collection<? extends GrantedAuthority> authorities = jwtUser.getAuthorities();
-        for (GrantedAuthority authority : authorities){
+        final Collection<? extends GrantedAuthority> authorities = jwtUser.getAuthorities();
+        for (final GrantedAuthority authority : authorities) {
             role = authority.getAuthority();
         }
 
-        String token = JwtTokenUtils.createToken(jwtUser.getUsername(), role, isRemember);
-//        String token = JwtTokenUtils.createToken(jwtUser.getUsername(), false);
+        final String token = JwtTokenUtils.createToken(jwtUser.getUsername(), role, isRemember);
+        // String token = JwtTokenUtils.createToken(jwtUser.getUsername(), false);
         // 返回创建成功的token
         // 但是这里创建的token只是单纯的token
         // 按照jwt的规定，最后请求的时候应该是 `Bearer token`
-        response.setHeader("token", JwtTokenUtils.TOKEN_PREFIX + token);
+        //response.setHeader("token", JwtTokenUtils.TOKEN_PREFIX + token);
+        response.getWriter().print("{taken:"+JwtTokenUtils.TOKEN_PREFIX + token+"}");
+        
     }
 
     @Override
-    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+    protected void unsuccessfulAuthentication(final HttpServletRequest request, final HttpServletResponse response,
+            final AuthenticationException failed) throws IOException, ServletException {
         response.getWriter().write("authentication failed, reason: " + failed.getMessage());
     }
 }
